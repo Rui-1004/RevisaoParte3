fdd9b66d10f56f3b3596e864652d874cce7974a1
Rui Silva <rui.silva1004@gmail.com>
2023-01-27 21:47:27 +0000

    edit

---
+++ RevisaoParte3/Data/Migrations/20230127214608_Versao03.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RevisaoParte3.Data.Migrations
+{
+    public partial class Versao03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
